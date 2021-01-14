c805d7c09eee3141bbeb89adca777c9dfd2d2941
Brandon <brandonbgrayson@gmail.com>
2021-01-13 23:27:19 -0500

    burger-list targets child buttons

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,9 +1,22 @@
+console.log("hello")
 $(function () {
     // click listener to fire on button click
-    $('#eat-burger').on("click", function (event) {
+    $('#burger-list').children("button").on("click", function (event) {
         event.preventDefault()
-        console.log(this.val())
-        var id = this.val
-    })
+        console.log(this.text())
+        var id = $(this).data("id");
+
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: {}
+        }).then(
+            function () {
+                console.log("changed devoured to", id)
+                location.reload()
+            }
+        )
+    });
+
+    // $(".create-form").on("submit", function (event))
 
 })
