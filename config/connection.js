1fdc0d5c1a58b7938c7e4a1c0d8683011e8eced1
Brandon <brandonbgrayson@gmail.com>
2021-01-14 09:20:48 -0500

    connected to Jawsdb for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,25 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-    host: "localhost",
+const connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
     
-    // Your port; if not 3306
-    port: process.env.PORT || 3306,
-    
-    // Your username
-    user: "root",
-    
-    // Your password
-    password: "Prolific1",
-    database: "burger_DB"
-});
+        // Your port; if not 3306
+        port: 3306,
+        
+        // Your username
+        user: "root",
+        
+        // Your password
+        password: "Prolific1",
+        database: "burger_DB"
+    })
+} 
+
     
     connection.connect(function(err) {
         if (err) throw err;
